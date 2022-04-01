81ba5783cf90bbb77e3921b793527c434f6e4632
TSC7 <michaelsantos1337@gmail.com>
2022-03-31 21:46:02 -0400

    Solved through steps 1-5.

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
 function computerPlay() {
   let x = ["rock", "paper", "scissors"];
   let returnChoice = x[Math.floor(Math.random()*x.length)];
-  console.log(returnChoice);
+  return String(returnChoice);
 }
+
+function playRound(playerSelection = playerSelect, computerSelection = computerSelect) {
+
+  console.log("You chose: " + playerSelection);
+  console.log("The computer chose: " + computerSelection);
+
+
+  if (playerSelection === computerSelection) {
+    return("Tie!");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return("You lose! Paper beats rock!");
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return("You win! Rock beats scissors!");
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return("You win! Paper beats rock!");
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return("You lose! Scissors beats paper!");
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return("You lose! Rock beats scissors!");
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return("You win! Scissors beats paper!");
+  }
+  else {
+    location.reload();
+    return("You must not have given a valid input; refresh the page and try again!");
+  }
+
+}
+
+
+let playerSelect = prompt("Enter rock, paper, or scissors").toLowerCase();
+let computerSelect = computerPlay();
+console.log(playRound(playerSelect, computerSelect));
